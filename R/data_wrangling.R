3a8dbd293d406b0e5ee1dd7ad3b67311460e9470
anyaferguson <anya.ferguson@nhs.net>
2024-07-01 11:38:26 +0100

    wrangling gender and age

--- R/data_wrangling.R
+++ R/data_wrangling.R
@@ -1,45 +1,24 @@
-# # To get data from a xls file at a URL. Default is to read sheet 1, but can
-# # specify other sheet and to not skip any rows.
-# scrape_xls <- function(url, sheet = 1, skip = 0) {
-#   tmp = tempfile(fileext = "")
-#
-#   download.file(url = url,
-#                 destfile = tmp,
-#                 mode = "wb")
-#
-#   data <- readxl::read_excel(path = tmp,
-#                              sheet = sheet,
-#                              skip = skip) |>
-#     janitor::clean_names()
-#
-#   return(data)
-#
-# }
-#
-# # To get data from the first excel file in a zipped folder at a URL. Default is
-# # to read sheet 1 and to not skip any rows.
-# scrape_zipped_xls <- function(url, sheet = 1, skip = 0) {
-#   download.file(url, "zippeddata.zip")
-#   unzip("zippeddata.zip")
-#
-#   filename <- unzip("zippeddata.zip", list = TRUE) |>
-#     dplyr::select(Name) |>
-#     dplyr::pull()
-#
-#   data <- readxl::read_excel(filename,
-#                              sheet = sheet,
-#                              skip = skip) |>
-#     janitor::clean_names()
-#
-#   unlink("zippeddata.zip", recursive = TRUE)
-#   unlink(filename)
-#
-#   return(data)
-#
-# }
+# To get data from a xls file at a URL. Default is to read sheet 1, but can
+# specify other sheet and to not skip any rows.
+scrape_xls <- function(url, sheet = 1, skip = 0) {
+  tmp = tempfile(fileext = "")
 
-get_file_from_url_zipped <- function(url){
+  download.file(url = url,
+                destfile = tmp,
+                mode = "wb")
 
+  data <- readxl::read_excel(path = tmp,
+                             sheet = sheet,
+                             skip = skip) |>
+    janitor::clean_names()
+
+  return(data)
+
+}
+
+# To get data from the first excel file in a zipped folder at a URL. Default is
+# to read sheet 1 and to not skip any rows.
+scrape_zipped_xls <- function(url, sheet = 1, skip = 0) {
   download.file(url, "zippeddata.zip")
   unzip("zippeddata.zip")
 
@@ -47,123 +26,286 @@ get_file_from_url_zipped <- function(url){
     dplyr::select(Name) |>
     dplyr::pull()
 
-  return(filename)
+  data <- readxl::read_excel(filename,
+                             sheet = sheet,
+                             skip = skip) |>
+    janitor::clean_names()
 
-}
-
-tidy_after_unzip <- function(filename){
   unlink("zippeddata.zip", recursive = TRUE)
   unlink(filename)
-}
-
-get_pop_by_gender_2018 <- function(url) {
-
-  filename <- get_file_from_url_zipped(url)
-
-  data <- data.frame()
-
-  for(i in c("Persons", "Females", "Males")) {
-
-    data_to_add <- readxl::read_excel(filename,
-                                      sheet = glue::glue("Mid-2018 {i}"),
-                                      skip = 4) |>
-      janitor::clean_names() |>
-      dplyr::mutate(type = i) |>
-      dplyr::filter(!is.na(lsoa)) |>
-      dplyr::select(lsoa_code = area_codes, all_ages, type)
-
-    data <- data |>
-      dplyr::bind_rows(data_to_add)
-  }
-
-  data <- data |>
-    tidyr::pivot_wider(names_from = "type",
-                       values_from = all_ages) |>
-    dplyr::mutate(year = "2018") |>
-    janitor::clean_names()
-
-  tidy_after_unzip(filename)
 
   return(data)
 
 }
 
-get_pop_by_gender <- function(filename, year){
 
-  data <- data.frame()
 
-  for(i in c("Persons", "Females", "Males")) {
-    data_to_add <- readxl::read_excel(filename,
-                                      sheet = glue::glue("Mid-{year} {i}"),
-                                      skip = 4) |>
-      janitor::clean_names() |>
-      dplyr::mutate(type = i) |>
-      dplyr::select(lsoa_code, all_ages, type)
+wrangle_gender_totals_18_20 <- function(data){
 
-    data <- data |>
-      dplyr::bind_rows(data_to_add)
-  }
+  name <- deparse(substitute(data))
 
-  data <- data |>
-    tidyr::pivot_wider(names_from = "type",
-                       values_from = all_ages) |>
-    dplyr::mutate(year = year) |>
-    janitor::clean_names()
+  year <- name |>
+    stringr::str_sub(12, 15)
 
-  return(data)
+  gender <- name |>
+    stringr::str_sub(17)
+
+  wrangled_data <- data |>
+    dplyr::mutate(year = year,
+                  gender = gender) |>
+    dplyr::select(year, lsoa_code, gender, count = all_ages)
+
+  return(wrangled_data)
 }
 
-get_pop_by_gender_2019 <- function(url, year) {
+wrangle_gender_totals_21_22 <- function(data){
 
-  filename <- get_file_from_url_zipped(url)
+  name <- deparse(substitute(data))
 
-  data <- get_pop_by_gender(filename, year)
+  year <- name |>
+    stringr::str_sub(12, 15)
 
-  tidy_after_unzip(filename)
+  wrangled_data <- data |>
+    dplyr::mutate(
+      females = raster::rowSums(dplyr::across(tidyselect::starts_with("f"))),
+      males = raster::rowSums(dplyr::across(tidyselect::starts_with("m"))),
+      year = year
+    ) |>
+    dplyr::select(year, lsoa_code = lsoa_2021_code, females, males) |>
+    tidyr::pivot_longer(names_to = "gender",
+                        values_to = "count",
+                        cols = c(females, males))
 
-  return(data)
-
-}
-
-get_file_from_url <- function(url){
-  filename <- tempfile(fileext = "")
-
-  download.file(url = url,
-                destfile = filename,
-                mode = "wb")
-
-  return(filename)
-}
-
-get_pop_by_gender_2020 <- function(url, year) {
-  filename <- get_file_from_url(url)
-
-  data <- get_pop_by_gender(filename, year)
-
-  return(data)
+  return(wrangled_data)
 
 }
 
-get_pop_by_gender_2021_22 <- function(url, year){
+get_gender_totals <- function(population_2018_females,
+                              population_2018_males,
+                              population_2019_females,
+                              population_2019_males,
+                              population_2020_females,
+                              population_2020_males,
+                              population_2021,
+                              population_2022) {
 
-  filename <- get_file_from_url(url)
+  combined <- rbind(wrangle_gender_totals_18_20(population_2018_females),
+             wrangle_gender_totals_18_20(population_2018_males),
+             wrangle_gender_totals_18_20(population_2019_females),
+             wrangle_gender_totals_18_20(population_2019_males),
+             wrangle_gender_totals_18_20(population_2020_females),
+             wrangle_gender_totals_18_20(population_2020_males),
+             wrangle_gender_totals_21_22(population_2021),
+             wrangle_gender_totals_21_22(population_2022)
+  )
 
-  data <- readxl::read_excel(filename,
-                             sheet = glue::glue("Mid-{year} LSOA 2021"),
-                             skip = 3)  |>
-    janitor::clean_names() |>
-    dplyr::mutate(females = rowSums(dplyr::across(tidyselect::starts_with("f"))),
-                  males = rowSums(dplyr::across(tidyselect::starts_with("m"))),
+  return(combined)
+}
+
+
+wrangle_age_totals_18_20 <- function(data){
+
+  name <- deparse(substitute(data))
+
+  year <- name |>
+    stringr::str_sub(12, 15)
+
+  wrangled_data <- data |>
+    tidyr::pivot_longer(names_to = "age",
+                        values_to = "count",
+                        cols = tidyselect::starts_with("x")) |>
+    dplyr::mutate(age = readr::parse_number(age),
+                  age_group = dplyr::case_when(age < 18 ~ "0-17",
+                                               age < 22 ~ "18-21",
+                                               age < 40 ~ "22-39",
+                                               age < 65 ~ "40-64",
+                                               age < 75 ~ "65-74",
+                                               age < 120 ~ "75+",
+                                               .default = "error"),
                   year = year) |>
-    dplyr::select(lsoa_code = lsoa_2021_code,
-                  "persons" = total,
-                  females,
-                  males,
-                  year)
+    dplyr::summarise(count = sum(count), .by = c(year, lsoa_code, age_group)) |>
+    dplyr::select(year, lsoa_code, age_group, count)
+
+  return(wrangled_data)
 
 }
 
+wrangle_age_totals_21_22 <- function(data){
+
+  name <- deparse(substitute(data))
+
+  year <- name |>
+    stringr::str_sub(12, 15)
 
 
+  wrangled_data <- data |>
+    tidyr::pivot_longer(names_to = "category",
+                        values_to = "count",
+                        cols = tidyselect::starts_with(c("f", "m"))) |>
+    dplyr::mutate(age = readr::parse_number(category),
+                  age_group = dplyr::case_when(age < 18 ~ "0-17",
+                                               age < 22 ~ "18-21",
+                                               age < 40 ~ "22-39",
+                                               age < 65 ~ "40-64",
+                                               age < 75 ~ "65-74",
+                                               age < 120 ~ "75+",
+                                               .default = "error"),
+                  year = year) |>
+    dplyr::summarise(count = sum(count),
+                     .by = c(year, age_group, lsoa_2021_code)) |>
+    dplyr::select(year, lsoa_code = lsoa_2021_code, age_group, count)
 
+  return(wrangled_data)
+}
 
+get_age_totals <- function(population_2018_females,
+                              population_2018_males,
+                              population_2019_females,
+                              population_2019_males,
+                              population_2020_females,
+                              population_2020_males,
+                              population_2021,
+                              population_2022
+                           ) {
+
+  combined <- rbind(wrangle_age_totals_18_20(population_2018_females),
+                    wrangle_age_totals_18_20(population_2018_males),
+                    wrangle_age_totals_18_20(population_2019_females),
+                    wrangle_age_totals_18_20(population_2019_males),
+                    wrangle_age_totals_18_20(population_2020_females),
+                    wrangle_age_totals_18_20(population_2020_males),
+                    wrangle_age_totals_21_22(population_2021),
+                    wrangle_age_totals_21_22(population_2022)
+  )
+
+  return(combined)
+}
+# get_file_from_url_zipped <- function(url){
+#
+#   download.file(url, "zippeddata.zip")
+#   unzip("zippeddata.zip")
+#
+#   filename <- unzip("zippeddata.zip", list = TRUE) |>
+#     dplyr::select(Name) |>
+#     dplyr::pull()
+#
+#   return(filename)
+#
+# }
+#
+# tidy_after_unzip <- function(filename){
+#   unlink("zippeddata.zip", recursive = TRUE)
+#   unlink(filename)
+# }
+#
+# get_pop_by_gender_2018 <- function(url) {
+#
+#   filename <- get_file_from_url_zipped(url)
+#
+#   data <- data.frame()
+#
+#   for(i in c("Persons", "Females", "Males")) {
+#
+#     data_to_add <- readxl::read_excel(filename,
+#                                       sheet = glue::glue("Mid-2018 {i}"),
+#                                       skip = 4) |>
+#       janitor::clean_names() |>
+#       dplyr::mutate(type = i) |>
+#       dplyr::filter(!is.na(lsoa)) |>
+#       dplyr::select(lsoa_code = area_codes, all_ages, type)
+#
+#     data <- data |>
+#       dplyr::bind_rows(data_to_add)
+#   }
+#
+#   data <- data |>
+#     tidyr::pivot_wider(names_from = "type",
+#                        values_from = all_ages) |>
+#     dplyr::mutate(year = "2018") |>
+#     janitor::clean_names()
+#
+#   tidy_after_unzip(filename)
+#
+#   return(data)
+#
+# }
+#
+# get_pop_by_gender <- function(filename, year){
+#
+#   data <- data.frame()
+#
+#   for(i in c("Persons", "Females", "Males")) {
+#     data_to_add <- readxl::read_excel(filename,
+#                                       sheet = glue::glue("Mid-{year} {i}"),
+#                                       skip = 4) |>
+#       janitor::clean_names() |>
+#       dplyr::mutate(type = i) |>
+#       dplyr::select(lsoa_code, all_ages, type)
+#
+#     data <- data |>
+#       dplyr::bind_rows(data_to_add)
+#   }
+#
+#   data <- data |>
+#     tidyr::pivot_wider(names_from = "type",
+#                        values_from = all_ages) |>
+#     dplyr::mutate(year = year) |>
+#     janitor::clean_names()
+#
+#   return(data)
+# }
+#
+# get_pop_by_gender_2019 <- function(url, year) {
+#
+#   filename <- get_file_from_url_zipped(url)
+#
+#   data <- get_pop_by_gender(filename, year)
+#
+#   tidy_after_unzip(filename)
+#
+#   return(data)
+#
+# }
+#
+# get_file_from_url <- function(url){
+#   filename <- tempfile(fileext = "")
+#
+#   download.file(url = url,
+#                 destfile = filename,
+#                 mode = "wb")
+#
+#   return(filename)
+# }
+#
+# get_pop_by_gender_2020 <- function(url, year) {
+#   filename <- get_file_from_url(url)
+#
+#   data <- get_pop_by_gender(filename, year)
+#
+#   return(data)
+#
+# }
+#
+# get_pop_by_gender_2021_22 <- function(url, year){
+#
+#   filename <- get_file_from_url(url)
+#
+#   data <- readxl::read_excel(filename,
+#                              sheet = glue::glue("Mid-{year} LSOA 2021"),
+#                              skip = 3)  |>
+#     janitor::clean_names() |>
+#     dplyr::mutate(females = rowSums(dplyr::across(tidyselect::starts_with("f"))),
+#                   males = rowSums(dplyr::across(tidyselect::starts_with("m"))),
+#                   year = year) |>
+#     dplyr::select(lsoa_code = lsoa_2021_code,
+#                   "persons" = total,
+#                   females,
+#                   males,
+#                   year)
+#
+# }
+#
+#
+#
+#
+#
--- _targets.R
+++ _targets.R
@@ -7,42 +7,95 @@ library(targets)
 # and tar_read(data_summary) to view the results.
 
 # Set target options:
-tar_option_set(
-  packages = c( # Packages that your targets need for their tasks.
-    "tidyverse"
-  )
-)
+tar_option_set(packages = c(# Packages that your targets need for their tasks.
+  "tidyverse"))
 
 # Run the R scripts in the R/ folder with your custom functions:
 tar_source()
 
 # End this file with a list of target objects.
 list(
-  tar_target(population_2021_22_url,
-             "https://www.ons.gov.uk/file?uri=/peoplepopulationandcommunity/populationandmigration/populationestimates/datasets/lowersuperoutputareamidyearpopulationestimates/mid2021andmid2022/sapelsoasyoatablefinal.xlsx"),
-  tar_target(population_2020_url,
-             "https://www.ons.gov.uk/file?uri=/peoplepopulationandcommunity/populationandmigration/populationestimates/datasets/lowersuperoutputareamidyearpopulationestimates/mid2020sape23dt2/sape23dt2mid2020lsoasyoaestimatesunformatted.xlsx"),
-  tar_target(population_2019_url,
-             "https://www.ons.gov.uk/file?uri=/peoplepopulationandcommunity/populationandmigration/populationestimates/datasets/lowersuperoutputareamidyearpopulationestimates/mid2019sape22dt2/sape22dt2mid2019lsoasyoaestimatesunformatted.zip"),
-  tar_target(population_2018_url,
-             "https://www.ons.gov.uk/file?uri=/peoplepopulationandcommunity/populationandmigration/populationestimates/datasets/lowersuperoutputareamidyearpopulationestimates/mid2018sape21dt1a/sape21dt1amid2018on2019lalsoasyoaestimatesformatted.zip"),
-
-  tar_target(pop_2018,
-             get_pop_by_gender_2018(population_2018_url)),
-  tar_target(pop_2019,
-             get_pop_by_gender_2019(population_2019_url, "2019")),
-  tar_target(pop_2020,
-             get_pop_by_gender_2020(population_2020_url, "2020")),
-  tar_target(pop_2021,
-             get_pop_by_gender_2021_22(population_2021_22_url, "2021")
+  # URLs for population data
+  tar_target(
+    url_population_2021_22,
+    "https://www.ons.gov.uk/file?uri=/peoplepopulationandcommunity/populationandmigration/populationestimates/datasets/lowersuperoutputareamidyearpopulationestimates/mid2021andmid2022/sapelsoasyoatablefinal.xlsx"
+  ),
+  tar_target(
+    url_population_2020,
+    "https://www.ons.gov.uk/file?uri=/peoplepopulationandcommunity/populationandmigration/populationestimates/datasets/lowersuperoutputareamidyearpopulationestimates/mid2020sape23dt2/sape23dt2mid2020lsoasyoaestimatesunformatted.xlsx"
+  ),
+  tar_target(
+    url_population_2019,
+    "https://www.ons.gov.uk/file?uri=/peoplepopulationandcommunity/populationandmigration/populationestimates/datasets/lowersuperoutputareamidyearpopulationestimates/mid2019sape22dt2/sape22dt2mid2019lsoasyoaestimatesunformatted.zip"
+  ),
+  tar_target(
+    url_population_2018,
+    "https://www.ons.gov.uk/file?uri=/peoplepopulationandcommunity/populationandmigration/populationestimates/datasets/lowersuperoutputareamidyearpopulationestimates/mid2018sape21dt1a/sape21dt1amid2018on2019lalsoasyoaestimatesformatted.zip"
+  ),
+  # Reading in population data
+  tarchetypes::tar_map(
+    list(sheetnames = c("persons", "females", "males")),
+    tar_target(
+      population_2018,
+      scrape_zipped_xls(
+        url_population_2018,
+        paste0("Mid-2018 ", stringr::str_to_title(sheetnames)),
+        4
+      ) |>
+        dplyr::rename(lsoa_code = area_codes)
+    )
+  ),
+  tarchetypes::tar_map(
+    list(sheetnames = c("persons", "females", "males")),
+    tar_target(
+      population_2019,
+      scrape_zipped_xls(
+        url_population_2019,
+        paste0("Mid-2019 ", stringr::str_to_title(sheetnames)),
+        4
+      )
+    )
+  ),
+  tarchetypes::tar_map(
+    list(sheetnames = c("persons", "females", "males")),
+    tar_target(population_2020, scrape_xls(
+      url_population_2020,
+      paste0("Mid-2020 ", stringr::str_to_title(sheetnames)),
+      4
+    ))
+  ),
+  tar_target(
+    population_2021,
+    scrape_xls(url_population_2021_22, "Mid-2021 LSOA 2021", 3)
+  ),
+  tar_target(
+    population_2022,
+    scrape_xls(url_population_2021_22, "Mid-2022 LSOA 2021", 3)
+  ),
+  # Population by gender
+  tar_target(gender_totals,
+             get_gender_totals(population_2018_females,
+                               population_2018_males,
+                               population_2019_females,
+                               population_2019_males,
+                               population_2020_females,
+                               population_2020_males,
+                               population_2021,
+                               population_2022)
              ),
-  tar_target(pop_2022,
-             get_pop_by_gender_2021_22(population_2021_22_url, "2022")),
-  tar_target(pop,
-             rbind(pop_2018,
-                   pop_2019,
-                   pop_2020,
-                   pop_2021,
-                   pop_2020))
+  # Population by age
+  tar_target(age_totals,
+             get_age_totals(population_2018_females,
+                               population_2018_males,
+                               population_2019_females,
+                               population_2019_males,
+                               population_2020_females,
+                               population_2020_males,
+                               population_2021,
+                               population_2022
+                               )
+  )
+
+
 
 )
