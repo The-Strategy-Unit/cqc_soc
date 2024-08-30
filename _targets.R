2af54e5982346d753c3d891377f7f7e7c6fbf940
anyaferguson <anya.ferguson@nhs.net>
2024-08-30 11:28:41 +0100

    percentage of redetentions

--- _targets.R
+++ _targets.R
@@ -442,17 +442,19 @@ list(
              get_111_mh_known(data_111)),
 
   ## Redetentions --------------------------------------------------------------
-  tar_target(cyp_redetentions_number,
+  tar_target(cyp_redetentions_perc,
              cyp_redetentions |>
-               dplyr::summarise(value = sum(attends),
-                                .by = c(der_financial_year, icb_code))),
+               dplyr::summarise(detentions = sum(detentions),
+                                attends = sum(attends),
+                                .by = c(der_financial_year, icb_code)) |>
+               dplyr::mutate(value = attends / detentions)),
   tar_target(
-    cyp_redetentions_boxplot,
-    get_standard_boxplot(cyp_redetentions_number)
+    cyp_redetentions_perc_boxplot,
+    get_standard_boxplot(cyp_redetentions_perc)
   ),
   tar_target(
-    cyp_redetentions_number_table,
-    get_icb_breakdown_table_redetentions(cyp_redetentions_number, icb_codes_names)
+    cyp_redetentions_perc_table,
+    get_icb_breakdown_table_redetentions(cyp_redetentions_perc, icb_codes_names)
   ),
 
   # 05. Breakdowns -------------------------------------------------------------
