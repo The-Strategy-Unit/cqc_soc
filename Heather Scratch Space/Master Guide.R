af10d41c0577a6210b4729be03d44fe7e06f73f1
heather-humphreys <154340810+heather-humphreys@users.noreply.github.com>
2024-09-11 12:38:36 +0100

    spacing issues?

---
+++ Heather Scratch Space/Master Guide.R
@@ -0,0 +1,26 @@
+# Master Guide on What to Do
+# Summary of notes for myself on what I need to know
+
+# Running Targets
+tar_make()
+
+
+# At the start of everyday: ####################################################
+
+# Load the relevant libraries:
+library(readr)
+library(tidyverse)
+library(targets)
+library(sf)
+
+
+# Step 2: Sync with project
+# Open R studio --> go to main --> pull the latest --> go to my branch --> merge the latest
+# then run:
+  usethis::pr_merge_main ()
+
+#Step 3:
+data <- tar_read(DTT)
+
+#Step 4:
+tar_make()
